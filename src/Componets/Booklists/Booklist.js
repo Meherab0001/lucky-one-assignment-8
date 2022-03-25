9f9cf5878438eb31f96a89c65a5a49f98bf07865
Meherab211 <meherab395@gmail.com>
2022-03-25 22:17:45 +0600

    testing

---
+++ src/Componets/Booklists/Booklist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Booklist = () => {
+    return (
+        <div>
+            <h2>This book list of famous writer in bd</h2>
+        </div>
+    );
+};
+
+export default Booklist;
